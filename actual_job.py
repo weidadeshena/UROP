a7e20dc55293a2821f32937f930ac9a16e2ad3a6
Cheryl <lovenpoison@gmail.com>
2019-06-26 11:29:03 +0100

    added orientation,doesn't work

--- actual_job.py
+++ actual_job.py
@@ -7,25 +7,23 @@ x_ori = np.array([[1,0,0]])
 y_ori = np.array([[0,1,0]])
 z_ori = np.array([[0,0,1]])
 origin = np.array([[0,0,0]])
-np.random.seed(5)
+# np.random.seed(5)
 sigma_r = 0.01
 sigma = 0.5
+phi = (np.random.rand()-0.5)*2*np.pi
+theta = (np.random.rand()-0.5)*np.pi/6
 number_of_measurements = 200
-animated = False
+
+def plane_rotation_matrix(phi,theta):
+    R_z = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
+    R_x = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
+    return np.dot(R_z,R_x)
+
 
 def quiver_data_to_segments(X, Y, Z, u, v, w, length=1):
     segments = (X, Y, Z, X+v*length, Y+u*length, Z+w*length)
     segments = np.array(segments).reshape(6,-1)
     return [[[x, y, z], [u, v, w]] for x, y, z, u, v, w in zip(*list(segments))]
-
-def random_basis():
-    x = np.random.rand(3)
-    x /= np.linalg.norm(x)
-    y = np.random.rand(3)
-    y -= y.dot(x)*x
-    y /= np.linalg.norm(y)
-    z = np.cross(x,y)
-    return x,y,z
 
 def random_position():
     x = (np.random.rand())*10
@@ -35,9 +33,9 @@ def random_position():
     return coord
 
 def random_rotation_matrix():
-    theta_z = (np.random.rand()-0.5)*np.pi/6
+    theta_z = (np.random.rand()-0.5)*np.pi/6 # only pi/12 rotation
     theta_y = (np.random.rand()-0.5)*np.pi/3
-    theta_x = (np.random.rand()-0.5)*np.pi/3 # only pi/12 rotation
+    theta_x = (np.random.rand()-0.5)*np.pi/3 
     R_z = np.array([[np.cos(theta_z),-np.sin(theta_z),0],[np.sin(theta_z),np.cos(theta_z),0],[0,0,1]])
     R_y = np.array([[np.cos(theta_y),0,np.sin(theta_y)],[0,1,0],[-np.sin(theta_y),0,np.cos(theta_y)]])
     R_x = np.array([[1,0,0],[0,np.cos(theta_x),-np.sin(theta_x)],[0,np.sin(theta_x),np.cos(theta_x)]])
@@ -58,7 +56,7 @@ def generate_measurements(ray_origin,x_ray,z_ray):
     for i in range(number_of_measurements):
         point_true = ray_origin + 10*np.random.rand()*x_ray + 10*np.random.rand()*z_ray
         point_noise = point_true + np.random.normal(0,sigma,size=(1,3))
-        measurements = np.vstack((measurements,point_noise[0]))
+        measurements = np.vstack((measurements,point_true[0]))
     return measurements
     
 
@@ -66,7 +64,7 @@ w_x = np.transpose(x_ori)
 w_y = np.transpose(y_ori)
 w_z = np.transpose(z_ori)
 
-theta_z,theta_y,theta_x,C_WT = random_rotation_matrix()
+C_WT = plane_rotation_matrix(phi,theta) 
 x_p,y_p,z_p = plane_frame(C_WT,w_x,w_y,w_z)
 plane_coord = random_position() # shape(1,3)
 
@@ -76,13 +74,15 @@ measurements = generate_measurements(plane_coord[0],np.transpose(x_p),np.transpo
 X,Y,Z = zip(origin[0],origin[0],origin[0],plane_coord[0],plane_coord[0],plane_coord[0])
 U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
 
-
-P_0 = np.diag(np.array([sigma_r,sigma_r,sigma_r])**2)
+theta_noise = theta+np.random.rand()*0.1
+phi_noise = phi+np.random.rand()*0.1
+P_0 = np.diag(np.array([sigma_r,sigma_r,sigma_r,sigma_r,sigma_r])**2)
 Q = P_0
-R = sigma**2*(C_WT[1][0]**2+C_WT[1][1]**2+C_WT[1][2]**2)
-H = np.dot(np.array([[0,1,0]]),np.transpose(C_WT)) # shape(1,3)
+
+# H = np.dot(np.array([[0,1,0]]),np.transpose(C_WT)) # shape(1,3)
 # initial state
 r_km1 = plane_coord + np.random.rand(1,3) # shape(1,3)
+x_km1 = np.array([np.append(r_km1,[theta_noise,phi_noise])])
 # initial covariance matrix
 P_km1 = P_0
 # print(C_WT)
@@ -137,31 +137,41 @@ P_km1 = P_0
 
 # print(plane_coord)
 
+# C_WT = C_z(phi)*C_x(theta)
+
 
 def animate(i):
-    global poses,points,r_km1,P_km1,x_p,y_p,z_p,x_ori,y_ori,z_ori
-    r_k_km1 = r_km1
+    global poses,points,r_km1,x_km1,P_km1,x_p,y_p,z_p,x_ori,y_ori,z_ori
+    x_k_km1 = x_km1
     P_k_km1 = P_km1 + Q
     # print("p shape is ", P_k_km1.shape)
     # measurement
-    y = np.dot(H,(np.transpose(np.array([measurements[i]])-r_k_km1))) #- np.linalg.norm(measurements[i])
-    print(abs(y))
+    # print(r_km1)
+    C_WT = plane_rotation_matrix(x_k_km1[0][3],x_k_km1[0][4]) 
+    x_p_est,y_p_est,z_p_est = plane_frame(C_WT,w_x,w_y,w_z)
+    H = np.array([[-np.cos(x_k_km1[0][3])*np.sin(x_k_km1[0][4]), np.cos(x_k_km1[0][3])*np.cos(x_k_km1[0][4]),np.sin(x_k_km1[0][3]),
+    np.sin(x_k_km1[0][3])*np.sin(x_k_km1[0][4])*(x_k_km1[0][1]-measurements[i][1])-np.sin(x_k_km1[0][3])*np.cos(x_k_km1[0][4])*(x_k_km1[0][2]-measurements[i][2])+np.cos(x_k_km1[0][3])*(x_k_km1[0][2]-measurements[i][2]),
+    -np.cos(x_k_km1[0][4])*np.cos(x_k_km1[0][3])*(x_k_km1[0][0]-measurements[i][0])-np.cos(x_k_km1[0][3])*np.sin(x_k_km1[0][4])*(x_k_km1[0][1]-measurements[i][1])+np.sin(x_k_km1[0][3])*np.sin(x_k_km1[0][4])]])
+    y = np.dot(np.dot(np.array([[0,1,0]]),np.transpose(C_WT)),(np.transpose(np.array([measurements[i]-x_k_km1[0][:3]])))) 
+    # print(abs(y))
     # print(measurements[i])
     # print(H)
     # print("y shape is ",y.shape)
+    R = sigma**2*(C_WT[1][0]**2+C_WT[1][1]**2+C_WT[1][2]**2)
     S = np.dot(H,P_k_km1).dot(np.transpose(H)) + R #(1,1)
     # print("s shape is ",S.shape)
-    K = np.dot(P_k_km1,np.transpose(H))/S #(3,1)
-    print(K)
+    K = np.dot(P_k_km1,np.transpose(H))/S #(5,1)
+    # print(K)
     # print("k shape is ",K.shape)
-    r_km1 = r_k_km1 + np.transpose(np.dot(K,y)) #(1,3)
-    P_km1 = np.dot((np.identity(3) - np.dot(K,H)),P_k_km1)
+    x_km1 = x_k_km1 + np.transpose(np.dot(K,y)) #(1,5)
+    P_km1 = np.dot((np.identity(5) - np.dot(K,H)),P_k_km1)
     # r_km1 = r_k_km1
     # P_km1 = P_k_km1
     # points.set_data(measurements[i][0],measurements[i][1])
     # points.set_3d_properties(measurements[i][2])
+    r_km1 = np.array([x_k_km1[0][:3]])
     X,Y,Z = zip(origin[0],origin[0],origin[0],r_km1[0],r_km1[0],r_km1[0],plane_coord[0],plane_coord[0],plane_coord[0])
-    U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
+    U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p_est)[0],np.transpose(y_p_est)[0],np.transpose(z_p_est)[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
     # segments = quiver_data_to_segments(X,Y,Z,U,V,W)
     # poses.set_segments(segments)
     poses.remove()
