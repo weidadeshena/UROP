aa3caded2e80a5f7c2dc71ba0674343cf491347b
Cheryl <lovenpoison@gmail.com>
2019-06-27 11:20:01 +0100

    still doesn't work

--- actual_job.py
+++ actual_job.py
@@ -11,7 +11,7 @@ origin = np.array([[0,0,0]])
 sigma_r = 0.01
 sigma = 0.5
 phi = (np.random.rand()-0.5)*2*np.pi
-theta = (np.random.rand()-0.5)*np.pi/6
+theta = (np.random.rand()-0.5)*np.pi/8
 number_of_measurements = 200
 
 def plane_rotation_matrix(phi,theta):
@@ -54,7 +54,7 @@ def plane_frame(R,w_x,w_y,w_z):
 def generate_measurements(ray_origin,x_ray,z_ray):
     measurements = np.array([ray_origin])
     for i in range(number_of_measurements):
-        point_true = ray_origin + 10*np.random.rand()*x_ray + 10*np.random.rand()*z_ray
+        point_true = ray_origin + 10*(np.random.rand()-0.5)*x_ray + 10*(np.random.rand()-0.5)*z_ray
         point_noise = point_true + np.random.normal(0,sigma,size=(1,3))
         measurements = np.vstack((measurements,point_true[0]))
     return measurements
@@ -79,6 +79,7 @@ P_0 = np.diag(np.array([sigma_r,sigma_r,sigma_r,sigma_r,sigma_r])**2)
 Q = P_0
 # H = np.dot(np.array([[0,1,0]]),np.transpose(C_WT)) # shape(1,3)
 # initial state
+R = sigma**2*(C_WT[0][1]**2+C_WT[1][1]**2+C_WT[2][1]**2)
 r_km1 = plane_coord + np.random.rand(1,3) # shape(1,3)
 theta_noise = theta+np.random.rand()*0.1
 phi_noise = phi+np.random.rand()*0.1
@@ -141,41 +142,35 @@ P_km1 = P_0
 
 
 def animate(i):
-    global poses,points,r_km1,x_km1,P_km1,x_p,y_p,z_p,x_ori,y_ori,z_ori
+    global poses,points,r_km1,P_km1,x_p,y_p,z_p,x_ori,y_ori,z_ori,x_km1
     x_k_km1 = x_km1
     P_k_km1 = P_km1 + Q
     # print("p shape is ", P_k_km1.shape)
-    # measurement
-    # print(r_km1)
     theta = x_k_km1[0][3]
     phi = x_k_km1[0][4]
+    # print(theta,phi)
     C_WT = plane_rotation_matrix(theta,phi) 
+    # print(C_WT)
     x_p_est,y_p_est,z_p_est = plane_frame(C_WT,w_x,w_y,w_z)
-    H = np.array([[-np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi),np.sin(theta),
-    np.sin(phi)*np.sin(theta)*(x_k_km1[0][0]-measurements[i][0])-np.sin(theta)*np.cos(phi)*(x_k_km1[0][1]-measurements[i][1])+np.cos(theta)*(x_k_km1[0][2]-measurements[i][2]),
-    -np.cos(theta)*np.cos(phi)*(x_k_km1[0][0]-measurements[i][0])-np.cos(theta)*np.sin(phi)*(x_k_km1[0][1]-measurements[i][1])+np.sin(x_k_km1[0][3])*np.sin(x_k_km1[0][4])]])
-    y = np.dot(np.dot(np.array([[0,1,0]]),np.transpose(C_WT)),(np.transpose(np.array([measurements[i]-x_k_km1[0][:3]])))) 
+    H = np.array([[-np.cos(theta)*np.sin(phi),np.cos(theta)*np.cos(phi),np.sin(theta),
+    np.sin(phi)*np.sin(theta)*x_k_km1[0][0]-np.sin(theta)*np.cos(phi)*x_k_km1[0][1]+np.cos(theta)*x_k_km1[0][2],
+    -np.cos(theta)*np.cos(phi)*x_k_km1[0][0]-np.cos(theta)*np.sin(phi)*x_k_km1[0][1]]])
+    y = np.dot(np.dot(np.array([[0,1,0]]),np.transpose(C_WT)),np.transpose(np.array([measurements[i]-x_k_km1[0][:3]]))) 
     # print(abs(y))
     # print(measurements[i])
     # print(H)
     # print("y shape is ",y.shape)
-    R = sigma**2*(C_WT[1][0]**2+C_WT[1][1]**2+C_WT[1][2]**2)
     S = np.dot(H,P_k_km1).dot(np.transpose(H)) + R #(1,1)
     # print("s shape is ",S.shape)
     K = np.dot(P_k_km1,np.transpose(H))/S #(5,1)
+    print(K)
     # print(K)
     # print("k shape is ",K.shape)
     x_km1 = x_k_km1 + np.transpose(np.dot(K,y)) #(1,5)
     P_km1 = np.dot((np.identity(5) - np.dot(K,H)),P_k_km1)
-    # r_km1 = r_k_km1
-    # P_km1 = P_k_km1
-    # points.set_data(measurements[i][0],measurements[i][1])
-    # points.set_3d_properties(measurements[i][2])
     r_km1 = np.array([x_k_km1[0][:3]])
     X,Y,Z = zip(origin[0],origin[0],origin[0],r_km1[0],r_km1[0],r_km1[0],plane_coord[0],plane_coord[0],plane_coord[0])
     U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p_est)[0],np.transpose(y_p_est)[0],np.transpose(z_p_est)[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
-    # segments = quiver_data_to_segments(X,Y,Z,U,V,W)
-    # poses.set_segments(segments)
     poses.remove()
     poses = ax.quiver(X,Y,Z,U,V,W,length=1.,normalize=True,color='rgbrgb')
     # points.remove()
