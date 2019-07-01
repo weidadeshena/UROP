0738815147269ee98b95b0f90d27c1523ed0f43a
Cheryl <lovenpoison@gmail.com>
2019-07-01 15:40:56 +0100

    works

--- actual_job.py
+++ actual_job.py
@@ -11,7 +11,8 @@ np.random.seed(5)
 sigma_r = 0.01
 sigma = 0.5
 number_of_measurements = 200
-animated = False
+a = np.linspace(0,5,number_of_measurements)
+b = np.random.normal(1,0.2,number_of_measurements)
 
 def quiver_data_to_segments(X, Y, Z, u, v, w, length=1):
     segments = (X, Y, Z, X+v*length, Y+u*length, Z+w*length)
@@ -60,6 +61,14 @@ def generate_measurements(ray_origin,x_ray,z_ray):
         point_noise = point_true + np.random.normal(0,sigma,size=(1,3))
         measurements = np.vstack((measurements,point_noise[0]))
     return measurements
+
+def measurements_from_motion(ray_origin,x_ray,z_ray):
+    measurements = np.array([ray_origin])
+    for i in range(number_of_measurements):
+        point_true = ray_origin + a[i]*x_ray + b[i]*z_ray
+        point_noise = point_true + np.random.normal(0,sigma,size=(1,3))
+        measurements = np.vstack((measurements,point_true[0]))
+    return measurements
     
 
 w_x = np.transpose(x_ori)
@@ -70,7 +79,7 @@ theta_z,theta_y,theta_x,C_WT = random_rotation_matrix()
 x_p,y_p,z_p = plane_frame(C_WT,w_x,w_y,w_z)
 plane_coord = random_position() # shape(1,3)
 
-measurements = generate_measurements(plane_coord[0],np.transpose(x_p),np.transpose(z_p))
+measurements = measurements_from_motion(plane_coord[0],np.transpose(x_p),np.transpose(z_p))
 
 
 X,Y,Z = zip(origin[0],origin[0],origin[0],plane_coord[0],plane_coord[0],plane_coord[0])
@@ -90,54 +99,6 @@ P_km1 = P_0
 
 
 
-
-
-
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.set_xlim([0,12])
-# ax.set_ylim([0,12])
-# ax.set_zlim([0,12])
-# ax.quiver(X,Y,Z,U,V,W,length=1,normalize=True,color='rgbrgb')
-# measurements_ = np.transpose(measurements)
-# ax.scatter(measurements_[0],measurements_[1],measurements_[2],s=5)
-
-# print(r_km1)
-# # measurements[i] shape (1,3)
-# for i in range(measurements.shape[0]):
-#     # prediction
-#     r_k_km1 = r_km1 + np.random.normal(0,sigma_r,size=(1,3))
-#     P_k_km1 = P_km1 + Q
-#     # print("P_k_km1 is ", P_k_km1)
-#     # print("p shape is ", P_k_km1.shape)
-#     # measurement
-#     y = np.dot(H,(np.transpose(np.array([measurements[i]])-r_k_km1))) #(1,1)
-#     # print("y is ",y)
-#     # print("measurement is ",measurements[i])
-#     # print("r_km1 is ", r_km1)
-#     # print(H)
-#     # print("y shape is ",y.shape)
-#     S = np.dot(H,P_k_km1).dot(np.transpose(H)) + R #(1,1)
-#     # print("S is ",S)
-#     # print("s shape is ",S.shape)
-#     K = np.dot(P_k_km1,np.transpose(H))/S #(3,1)
-#     # print("K is ",K)
-#     # print("k shape is ",K.shape)
-#     # print("np.transpose(np.dot(K,y)) ", np.transpose(np.dot(K,y)))
-#     # print("r_k_km1 ", r_k_km1)
-#     r_k_k = r_k_km1 + np.transpose(np.dot(K,y)) #(1,3)
-#     # print("r_k_k ",r_k_k)
-#     P_k_k = np.dot((np.identity(3) - np.dot(K,H)),P_k_km1)
-#     r_km1 = r_k_k
-#     P_km1 = P_k_k
-#     # r_km1 = r_k_km1
-#     # P_km1 = P_k_km1
-#     print(r_km1)
-
-
-# print(plane_coord)
-
-
 def animate(i):
     global poses,points,r_km1,P_km1,x_p,y_p,z_p,x_ori,y_ori,z_ori
     r_k_km1 = r_km1
@@ -152,7 +113,7 @@ def animate(i):
     S = np.dot(H,P_k_km1).dot(np.transpose(H)) + R #(1,1)
     # print("s shape is ",S.shape)
     K = np.dot(P_k_km1,np.transpose(H))/S #(3,1)
-    print(K)
+    # print(K)
     # print("k shape is ",K.shape)
     r_km1 = r_k_km1 + np.transpose(np.dot(K,y)) #(1,3)
     P_km1 = np.dot((np.identity(3) - np.dot(K,H)),P_k_km1)
