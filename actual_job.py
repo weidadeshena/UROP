01304357c6a8fb244395ae5d23da2cf60059c8e0
Cheryl <lovenpoison@gmail.com>
2019-07-02 13:53:00 +0100

    changed something

--- actual_job.py
+++ actual_job.py
@@ -10,22 +10,19 @@ origin = np.array([[0,0,0]])
 # np.random.seed(5)
 sigma_r = 0.01
 sigma = 0.5
-phi = (np.random.rand()-0.5)*2*np.pi
-theta = (np.random.rand()-0.5)*np.pi/8
+psi = (np.random.rand()-0.5)*2*np.pi
+phi = (np.random.rand()-0.5)*np.pi/8
+theta = 0
 number_of_measurements = 500
 a = np.linspace(0,5,number_of_measurements)
 b = np.random.normal(1,0.2,number_of_measurements)
 
-def plane_rotation_matrix(phi,theta):
-    R_z = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
-    R_x = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
+def plane_rotation_matrix(phi,psi):
+    R_z = np.array([[np.cos(psi),-np.sin(psi),0],[np.sin(psi),np.cos(psi),0],[0,0,1]])
+    R_x = np.array([[1,0,0],[0,np.cos(phi),-np.sin(phi)],[0,np.sin(phi),np.cos(phi)]])
+    # R_y = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta),0,np.cos(theta)]])
     return np.dot(R_z,R_x)
 
-
-def quiver_data_to_segments(X, Y, Z, u, v, w, length=1):
-    segments = (X, Y, Z, X+v*length, Y+u*length, Z+w*length)
-    segments = np.array(segments).reshape(6,-1)
-    return [[[x, y, z], [u, v, w]] for x, y, z, u, v, w in zip(*list(segments))]
 
 def random_position():
     x = (np.random.rand())*10
@@ -35,14 +32,14 @@ def random_position():
     return coord
 
 def random_rotation_matrix():
-    theta_z = (np.random.rand()-0.5)*np.pi/6 # only pi/12 rotation
-    theta_y = (np.random.rand()-0.5)*np.pi/3
-    theta_x = (np.random.rand()-0.5)*np.pi/3 
-    R_z = np.array([[np.cos(theta_z),-np.sin(theta_z),0],[np.sin(theta_z),np.cos(theta_z),0],[0,0,1]])
-    R_y = np.array([[np.cos(theta_y),0,np.sin(theta_y)],[0,1,0],[-np.sin(theta_y),0,np.cos(theta_y)]])
-    R_x = np.array([[1,0,0],[0,np.cos(theta_x),-np.sin(theta_x)],[0,np.sin(theta_x),np.cos(theta_x)]])
+    phi_z = (np.random.rand()-0.5)*np.pi/6 # only pi/12 rotation
+    phi_y = (np.random.rand()-0.5)*np.pi/3
+    phi_x = (np.random.rand()-0.5)*np.pi/3 
+    R_z = np.array([[np.cos(phi_z),-np.sin(phi_z),0],[np.sin(phi_z),np.cos(phi_z),0],[0,0,1]])
+    R_y = np.array([[np.cos(phi_y),0,np.sin(phi_y)],[0,1,0],[-np.sin(phi_y),0,np.cos(phi_y)]])
+    R_x = np.array([[1,0,0],[0,np.cos(phi_x),-np.sin(phi_x)],[0,np.sin(phi_x),np.cos(phi_x)]])
     R_total = np.dot(np.dot(R_z,R_y),R_x)
-    return theta_z,theta_y,theta_x,R_total
+    return phi_z,phi_y,phi_x,R_total
 
 def plane_frame(R,w_x,w_y,w_z):
     w_x_p = np.dot(R,w_x)
@@ -74,7 +71,7 @@ w_x = np.transpose(x_ori)
 w_y = np.transpose(y_ori)
 w_z = np.transpose(z_ori)
 
-C_WT = plane_rotation_matrix(phi,theta) 
+C_WT = plane_rotation_matrix(phi,psi) 
 x_p,y_p,z_p = plane_frame(C_WT,w_x,w_y,w_z)
 plane_coord = random_position() # shape(1,3)
 
@@ -85,20 +82,20 @@ X,Y,Z = zip(origin[0],origin[0],origin[0],plane_coord[0],plane_coord[0],plane_co
 U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
 
 
-Pr_0 = np.diag(np.array([sigma_r,sigma_r,sigma_r,sigma_r,sigma_r])**2)
+Pr_0 = np.diag(np.array([sigma_r,sigma_r,sigma_r,0.02,0.02])**2)
 Q = Pr_0
 H = np.dot(np.array([[0,1,0]]),np.transpose(C_WT)) # shape(1,3)
 # initial state
 
 r_km1 = plane_coord + np.random.rand(1,3) # shape(1,3)
-theta_noise = theta+np.random.rand()*0.05
-phi_noise = phi+np.random.rand()*0.05
-x_km1 = np.array([np.append(r_km1,[theta_noise,phi_noise])])
+phi_noise = phi+np.random.rand()*0.1
+psi_noise = psi+np.random.rand()*0.1
+x_km1 = np.array([np.append(r_km1,[phi_noise,psi_noise])])
 # initial covariance matrix
 P_km1 = Pr_0
 # print(C_WT)
 
-angle_km1 = np.array([[theta_noise,phi_noise]])
+angle_km1 = np.array([[phi_noise,psi_noise]])
 Pa_0 = np.diag(np.array([sigma_r,sigma_r])**2)
 Qa = Pa_0
 Pa_km1 = Pa_0
@@ -108,10 +105,12 @@ Pa_km1 = Pa_0
 #     global angle_km1,Pa_km1,poses,points
 #     angle_k_km1 = angle_km1
 #     Pa_k_km1 = Pa_km1 + Qa
-#     C_WT = plane_rotation_matrix(angle_km1[0][0],angle_km1[0][1])
+#     phi = angle_km1[0][0]
+#     psi = angle_km1[0][1]
+#     C_WT = plane_rotation_matrix(phi,psi)
 #     x_p_est,y_p_est,z_p_est = plane_frame(C_WT,w_x,w_y,w_z)
-#     H = np.array([[np.sin(phi)*np.sin(theta)*plane_coord[0][0]-np.sin(theta)*np.cos(phi)*plane_coord[0][1]+np.cos(theta)*plane_coord[0][2],
-#     -np.cos(theta)*np.cos(phi)*plane_coord[0][0]-np.cos(theta)*np.sin(phi)*plane_coord[0][1]]])
+#     H = np.array([[np.sin(psi)*np.sin(phi)*plane_coord[0][0]-np.sin(phi)*np.cos(psi)*plane_coord[0][1]+np.cos(phi)*plane_coord[0][2],
+#     -np.cos(phi)*np.cos(psi)*plane_coord[0][0]-np.cos(phi)*np.sin(psi)*plane_coord[0][1]]])
 #     y = np.dot(np.array([[0,1,0]]),np.dot(np.transpose(C_WT),np.transpose(np.array([measurements[i]-plane_coord[0]]))))
 #     print(y)
 #     R = sigma**2*(C_WT[0][1]**2+C_WT[1][1]**2+C_WT[2][1]**2)
@@ -134,16 +133,17 @@ def animate(i):
     x_k_km1 = x_km1
     P_k_km1 = P_km1 + Q
     # print("p shape is ", P_k_km1.shape)
-    theta_est = x_k_km1[0][3]
-    phi_est = x_k_km1[0][4]
-    C_WT = plane_rotation_matrix(theta_est,phi_est) 
+    phi_est = x_k_km1[0][3]
+    psi_est = x_k_km1[0][4]
+    # theta_est = x_k_km1[0][5]
+    C_WT = plane_rotation_matrix(phi_est,psi_est) 
     # print(C_WT)
     x_p_est,y_p_est,z_p_est = plane_frame(C_WT,w_x,w_y,w_z)
     # print("y_p is ", y_p)
     # print("y_p estimate is ", y_p_est)
-    H = np.array([[-np.cos(theta_est)*np.sin(phi_est),np.cos(theta_est)*np.cos(phi_est),np.sin(theta_est),
-    np.sin(phi_est)*np.sin(theta_est)*x_k_km1[0][0]-np.sin(theta_est)*np.cos(phi_est)*x_k_km1[0][1]+np.cos(theta_est)*x_k_km1[0][2],
-    -np.cos(theta_est)*np.cos(phi_est)*x_k_km1[0][0]-np.cos(theta_est)*np.sin(phi_est)*x_k_km1[0][1]]])
+    H = np.array([[-np.cos(phi_est)*np.sin(psi_est),np.cos(phi_est)*np.cos(psi_est),np.sin(phi_est),
+    np.sin(psi_est)*np.sin(phi_est)*x_k_km1[0][0]-np.sin(phi_est)*np.cos(psi_est)*x_k_km1[0][1]+np.cos(phi_est)*x_k_km1[0][2],
+    -np.cos(phi_est)*np.cos(psi_est)*x_k_km1[0][0]-np.cos(phi_est)*np.sin(psi_est)*x_k_km1[0][1]]])
     y = np.dot(np.array([[0,1,0]]),np.dot(np.transpose(C_WT),np.transpose(np.array([measurements[i]-x_k_km1[0][:3]]))))
     # y = np.dot(np.array([[0,1,0]]),np.dot(np.transpose(C_WT),np.transpose(np.array([measurements[i]-r_km1[0]]))))
     print(y) 
@@ -161,7 +161,8 @@ def animate(i):
     P_km1 = np.dot((np.identity(5) - np.dot(K,H)),P_k_km1)
     r_km1 = np.array([x_k_km1[0][:3]])
     X,Y,Z = zip(origin[0],origin[0],origin[0],r_km1[0],r_km1[0],r_km1[0],plane_coord[0],plane_coord[0],plane_coord[0])
-    U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p_est)[0],np.transpose(y_p_est)[0],np.transpose(z_p_est)[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
+    U,V,W = zip(x_ori[0],y_ori[0],z_ori[0],np.transpose(x_p_est)[0],np.transpose(y_p_est)[0],
+        np.transpose(z_p_est)[0],np.transpose(x_p)[0],np.transpose(y_p)[0],np.transpose(z_p)[0])
     poses.remove()
     poses = ax.quiver(X,Y,Z,U,V,W,length=1.,normalize=True,color='rgbrgb')
     # points.remove()
--- jacobian.py
+++ jacobian.py
@@ -8,28 +8,46 @@ ry = np.random.rand()*10
 rz = np.random.rand()*10
 delta = 0.01
 
-h=-np.sin(phi)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi)*ry+np.sin(theta)*rz
+def plane_rotation_matrix(theta,phi):
+    R_z = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
+    R_x = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
+    return np.dot(R_z,R_x)
+
+# print("C_WT: ")
+# print(plane_rotation_matrix(theta,phi))
+# C_WT = np.array([[np.cos(phi),-np.sin(phi)*np.cos(theta),np.sin(theta)*np.sin(phi)],[np.sin(phi),np.cos(phi)*np.cos(theta),-np.sin(theta)*np.cos(phi)],[0,np.sin(theta),np.cos(theta)]])
+# print(C_WT)
+# print(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))))
+# print(np.array([-np.sin(phi)*np.cos(theta),np.cos(phi)*np.cos(theta),np.sin(theta)]))
+h=np.dot(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))),np.array([rx,ry,rz]))
+print(h)
+k = -np.sin(phi)*np.cos(theta)*rx+np.cos(phi)*np.cos(theta)*ry+np.sin(theta)*rz
+print(k)
 # theta:
-numerial_theta = (-np.sin(phi)*np.cos(theta+delta)*rx+np.cos(theta+delta)*np.cos(phi)*ry+np.sin(theta+delta)*rz - (-np.sin(phi)*np.cos(theta-delta)*rx+np.cos(theta-delta)*np.cos(phi)*ry+np.sin(theta-delta)*rz))/0.02
+numerial_theta = (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta+delta,phi))).dot(np.array([rx,ry,rz]))-(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta-delta,phi))).dot(np.array([rx,ry,rz]))))/0.02
 differential_theta = np.sin(phi)*np.sin(theta)*rx-np.sin(theta)*np.cos(phi)*ry+np.cos(theta)*rz
-print("theta difference: ",numerial_theta-differential_theta)
+print("theta: ",numerial_theta,differential_theta)
 
 # phi:
-numerial_phi = (-np.sin(phi+delta)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi+delta)*ry+np.sin(theta)*rz - (-np.sin(phi-delta)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi-delta)*ry+np.sin(theta)*rz))/0.02
+numerial_phi = (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi+delta))).dot(np.array([rx,ry,rz]))-(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi-delta))).dot(np.array([rx,ry,rz]))))/0.02
 differential_phi = -np.cos(phi)*np.cos(theta)*rx-np.sin(phi)*np.cos(theta)*ry
-print("phi difference: ",numerial_phi-differential_phi)
+print("phi: ",numerial_phi,differential_phi)
 
 # rx:
-numerial_rx = (-np.sin(phi)*np.cos(theta)*(rx+delta)+np.cos(theta)*np.cos(phi)*ry+np.sin(theta)*rz - (-np.sin(phi)*np.cos(theta)*(rx-delta)+np.cos(theta)*np.cos(phi)*ry+np.sin(theta)*rz))/0.02
+numerial_rx = (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx+delta,ry,rz])) - (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx-delta,ry,rz]))))/0.02
 differential_rx = -np.sin(phi)*np.cos(theta)
-print("rx difference: ",numerial_rx-differential_rx)
+print("rx: ",numerial_rx,differential_rx)
 
 # ry:
-numerial_ry = (-np.sin(phi)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi)*(ry+delta)+np.sin(theta)*rz-(-np.sin(phi)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi)*(ry-delta)+np.sin(theta)*rz))/0.02
+numerial_ry = (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx,ry+delta,rz]))-(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx,ry-delta,rz]))))/0.02
 differential_ry = np.cos(theta)*np.cos(phi)
-print("ry difference: ",numerial_ry-numerial_ry)
+print("ry: ",numerial_ry,numerial_ry)
 
 # rz:
-numerial_rz = (-np.sin(phi)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi)*ry+np.sin(theta)*(rz+delta)-(-np.sin(phi)*np.cos(theta)*rx+np.cos(theta)*np.cos(phi)*ry+np.sin(theta)*(rz-delta)))/0.02
+numerial_rz = (np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx,ry,rz+delta]))-(np.dot(np.array([[0,1,0]]),np.transpose(plane_rotation_matrix(theta,phi))).dot(np.array([rx,ry,rz-delta]))))/0.02
 differential_rz = np.sin(theta)
-print("rz difference: ",numerial_rz-differential_rz)
+print("rz: ",numerial_rz,differential_rz)
+
+
+
+
