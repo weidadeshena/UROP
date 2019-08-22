844faa3ed5b0edc8598edf477eb4daaed69eae1c
Cheryl <lovenpoison@gmail.com>
2019-08-22 17:28:53 +0100

    EKF cpp works,spent a whole day wow

--- src/white_board_estimator.cpp
+++ src/white_board_estimator.cpp
@@ -1,52 +1,112 @@
 // EKF for white board estimation
 #include "white_board_estimator.h"
 // for generating random number
-#include <cstdlib>
+
 #include <ctime>
 #include <Eigen/Cholesky>
+#include <iostream>
+#include <stdio.h>
 
-using namespace Eigen;
-using namespace std;
 
-void whiteBoardEstimator::getBoardRotationMatrix(const float phi, const float psi,Matrix3d &C_WT)
+
+void whiteBoardEstimator::getBoardRotationMatrix(const double phi_, const double psi_, 
+												Eigen::Matrix3d &C_WT_)
 {
-	Matrix3d C_z,C_x;
-	C_z << cos(psi), sin(psi), 0,
-				-sin(psi), cos(psi), 0,
-					0, 0, 1;
-	C_x << 1, 0, 0,
-				0, cos(phi), sin(phi),
-				0, -sin(phi), cos(phi);
-	C_WT = C_z * C_x;
+	Eigen::Matrix3d C_z,C_x;
+	C_z <<  cos(psi_), sin(psi_), 0,
+		    -sin(psi_), cos(psi_), 0,
+		    0, 0, 1;
+	C_x <<  1, 0, 0,
+			0, cos(phi_), sin(phi_),
+			0, -sin(phi_), cos(phi_);
+	C_WT_ = C_z * C_x;
 }
 
-void whiteBoardEstimator::initialisation()
+Eigen::Vector3d whiteBoardEstimator::generateMeasurement()
+{
+	Eigen::Vector3d Measurement;
+	double x,z;
+	x = static_cast<double>(rand())/static_cast<double>(RAND_MAX/2);
+	z = static_cast<double>(rand())/static_cast<double>(RAND_MAX/1);
+	Eigen::Vector3d randXZVect(x,0.0,z);
+	Eigen::Matrix3d L = R_.llt().matrixL();
+	Eigen::Vector3d randVec = (Eigen::Vector3d::Random().array()+1)/2;
+	Measurement = w_r_true_ + C_WT_true_ * randXZVect; //+ L * randVec;
+	return Measurement;
+}
+
+void whiteBoardEstimator::EKF_initialisation()
 {
 	srand (static_cast <unsigned> (time(0)));
-	float LO = -0.1, HI = 0.1;
-	phi_true = LO + static_cast <float> (rand()) / static_cast <float> (RAND_MAX/(HI-LO));
-	LO = -3.1415926;
-	HI = 3.1415926;
-	psi_true = LO + static_cast <float> (rand()) / static_cast <float> (RAND_MAX/(HI-LO));
-	w_r_true = (Vector3d::Random()+Vector3d(1,1,1))*5.0;
-	x << w_r_true(0), w_r_true(1), w_r_true(2), phi_true, psi_true;
-	Matrix<double,5,5> L = P.llt().matrixL();
-	Matrix<double,5,1> randVec = Matrix<double,5,1>::Random();
-	randVec = (randVec + Matrix<double,5,1>::Constant(1.0))/2.0;
-	x = x + L * randVec;
+	double LO = -0.1, HI = 0.1;
+	phi_true_ = LO + static_cast<double>(rand()) / static_cast <double> (RAND_MAX/(HI-LO));
+	LO = -3.1415926; HI = 3.1415926;
+	psi_true_ = LO + static_cast<double>(rand()) / static_cast <double> (RAND_MAX/(HI-LO));
+	w_r_true_ = (Eigen::Vector3d::Random()+Eigen::Vector3d(1,1,1))*5.0;
+	this->getBoardRotationMatrix(phi_true_,psi_true_,C_WT_true_);
+	 x_true_ << w_r_true_(0), w_r_true_(1), w_r_true_(2), phi_true_, psi_true_;
+	Eigen::Matrix<double,5,5> L = P_.llt().matrixL();
+	Eigen::Matrix<double,5,1> randVec = (Eigen::Matrix<double,5,1>::Random().array()+1)/2;
+	 x_ = x_true_ + L * randVec;
 }
 
+void whiteBoardEstimator::EKF_predict(Eigen::Matrix<double,5,5> &P_)
+{
+	P_ = P_ + Q_;
+	
+}
+
+void whiteBoardEstimator::EKF_update(Eigen::Matrix<double,5,1> &x_, Eigen::Matrix<double,5,5> &P_)
+{
+	double phi_est = x_(3);
+	double psi_est = x_(4);
+	this->getBoardRotationMatrix(phi_est,psi_est,C_WT_);
+	Eigen::Vector3d Measurement;
+	Measurement = this->generateMeasurement();
+	Eigen::Vector3d delta_r = Measurement - x_.head<3>();
+	double H_phi = delta_r(2)*cos(phi_est) - delta_r(1)*cos(psi_est)*sin(phi_est)
+									+ delta_r(0)*sin(phi_est)*sin(psi_est);
+	double H_psi = -cos(phi_est)*(delta_r(0)*cos(psi_est) - delta_r(1)*sin(psi_est));
+	Eigen::Vector3d H_r = w_y_.transpose() * C_WT_.transpose();
+	Eigen::Matrix<double,5,1> H;
+	H << H_r(0), H_r(1), H_r(2), H_phi, H_psi;
+	// std::cout << "H:\n" << H << "\n";
+	double y = w_y_.transpose() * (C_WT_.transpose() * delta_r);
+	double S = H.transpose() * P_ * H + pow(sigma_r_,2) * 
+				(pow(C_WT_(1,0),2) + pow(C_WT_(1,1),2) + pow(C_WT_(1,2),2));
+	Eigen::Matrix<double,5,1> K = P_ * H /S;
+	Eigen::Matrix<double,5,1> delta_x = K * y;
+	// projection along normal direction
+	Eigen::Matrix3d s = (C_WT_ * w_y_) * delta_x.head<3>().transpose();
+	Eigen::Vector3d delta_x_temp = s * C_WT_ * w_y_;
+	delta_x(0) = delta_x_temp(0); delta_x(1) = delta_x_temp(1); delta_x(2) = delta_x_temp(2);
+  x_ = x_ + delta_x;
+	P_ = P_ - K * H.transpose() * P_;
+}
+
+bool whiteBoardEstimator::perform_EKF()
+{
+	this->EKF_predict(P_);
+	this->EKF_update(x_,P_);
+	return true;
+}
 
 int main()
 {
+	srand(28);
   whiteBoardEstimator WBE;
-  WBE.initialisation();
-  // Matrix<double,5,1> PVec;
-  // PVec << 0.01,0.01,0.01,0.01,0.01;
-  // Matrix<double,5,5> P = PVec.asDiagonal();
-  // Matrix<double,5,5> L = P.llt().matrixL();
-  // std::cout<< L <<std::endl;
-  std::cout << WBE.get_x() << std::endl;
+  WBE.EKF_initialisation();
+
+  for(int i=0; i<50; i++)
+  {
+  	WBE.perform_EKF();
+  }
+  Eigen::Matrix<double,5,1> x, x_true;
+  x = WBE.get_x();
+  x_true = WBE.get_x_true();
+
+  std::cout << "x:" << x << "\n";
+  std::cout << "x_true:" << x_true << std::endl;
 }
 
 
--- src/white_board_estimator.h
+++ src/white_board_estimator.h
@@ -1,9 +1,11 @@
-#include <iostream>
+#ifndef WHITE_BOARD_ESTIMATOR_H_
+#define WHITE_BOARD_ESTIMATOR_H_
+
+
 #include <Eigen/Dense>
 #include <cmath>
 
 
-using namespace Eigen;
 
 
 class whiteBoardEstimator
@@ -11,62 +13,77 @@ class whiteBoardEstimator
 	public:
 		whiteBoardEstimator() 
 		{
-			w_x << 1,0,0;
-			w_y << 0,1,0;
-			w_z << 0,0,1;
-			origin << 0,0,0;
-			sigma_r = 0.01;
-			sigma_q_r = 0.001;
-			sigma_angle = 0.001;
-			dt = 0.05;
-			N = 100;
-			Matrix<double,5,1> Qvector;
-			Qvector << pow(sigma_q_r,2),pow(sigma_q_r,2),pow(sigma_q_r,2),pow(sigma_angle,2),pow(sigma_angle,2);
-			Q = dt*Qvector.asDiagonal();
-			Matrix<double,5,1> Pvector;
-			Pvector << pow(sigma_r,2),pow(sigma_r,2),pow(sigma_r,2),pow(sigma_r,2),pow(sigma_r,2);
-			P = Pvector.asDiagonal();
+			w_x_ << 1,0,0;
+			w_y_ << 0,1,0;
+			w_z_ << 0,0,1;
+			origin_ << 0,0,0;
+			sigma_r_ = 0.01;
+			sigma_q_r_ = 0.001;
+			sigma_angle_ = 0.001;
+			dt_ = 0.05;
+			N_ = 100;
+			Eigen::Matrix<double,5,1> Qvector;
+			Qvector << pow(sigma_q_r_,2),pow(sigma_q_r_,2),pow(sigma_q_r_,2),pow(sigma_angle_,2),pow(sigma_angle_,2);
+			Q_ = dt_*Qvector.asDiagonal();
+			Eigen::Matrix<double,5,1> Pvector;
+			Pvector << 0.1,0.1,0.1,0.1,0.1;
+			P_ = Pvector.asDiagonal();
+			Eigen::Vector3d Rvector(pow(sigma_r_,2),pow(sigma_r_,2),pow(sigma_r_,2));
+			R_ = Rvector.asDiagonal();
 		};
 		//~whiteBoardEstimator();
-		float get_sigma_r() const {return sigma_r;};
-		Matrix<double,5,5> get_Q() const {return Q;};
-		Matrix<double,5,1> get_x() const {return x;};
-
-		// initialise the ground truth
-		void initialisation();
+		Eigen::Matrix<double,5,5> get_P() const {return P_;};
+		Eigen::Matrix<double,5,1> get_x() const {return x_;};
+		Eigen::Matrix<double,5,1> get_x_true() const {return x_true_;};
+		Eigen::Matrix3d get_C_WT() const {return C_WT_;};
+		Eigen::Matrix3d get_C_WT_true() const {return C_WT_true_;};
 
 		// calculate the rotation matrix given angle phi and psi
 		// param in: phi, psi
 		// param out: C_WT
-		void getBoardRotationMatrix(const float phi, const float psi,Matrix3d &C_WT);
+		void getBoardRotationMatrix(const double phi_, const double psi_, Eigen::Matrix3d &C_WT_);
+
+		// generate a measurement
+		Eigen::Vector3d generateMeasurement();
 
 		// kalman filter to estimate the plane
-		void EKF(Matrix<double,5,1> x);
+		// initialise the ground truth
+		void EKF_initialisation();
+		// predict step
+		void EKF_predict(Eigen::Matrix<double,5,5> &P_);
+		// update step
+		void EKF_update(Eigen::Matrix<double,5,1> &x_, Eigen::Matrix<double,5,5> &P_);
+		// perform EKF
+		bool perform_EKF();
+
+
 	private:
 		// Ground truth
 		// x, y, z axis in world frame
-		Vector3d w_x,w_y,w_z,origin;
+		Eigen::Vector3d w_x_,w_y_,w_z_,origin_;
 
 		// variables for covariance matrix
-		float sigma_r,sigma_q_r,sigma_angle;
-		Matrix<double,5,5> P,Q;
-		float dt;
-		int N;
+		float sigma_r_,sigma_q_r_,sigma_angle_;
+		Eigen::Matrix<double,5,5> P_,Q_;
+		Eigen::Matrix3d R_;
+		float dt_;
+		int N_;
 
 		// state variable x
-		Matrix<double,5,1> x;
+		Eigen::Matrix<double,5,1> x_,x_true_;
 		
 		// rotation matrix from world to plane frame
-		Matrix3d C_WT;  
+		Eigen::Matrix3d C_WT_,C_WT_true_;  
 		
 		// x, y, z axis on plane frame and position of plane in world frame
-		Vector3d w_p_x,w_p_y,w_p_z,w_r,w_r_true;
+		Eigen::Vector3d w_p_x_,w_p_y_,w_p_z_,w_r_,w_r_true_;
 
 		// euler angle of the plane
-		float phi,psi,phi_true,psi_true;
+		double phi_,psi_,phi_true_,psi_true_;
 };
 
 
 
 
 
+#endif  //WHITE_BOARD_ESTIMATOR_H_
